707407563b0cc9d18ee0cf0921bfe290acb0c26b
AliObaji <ali-obaji@hotmail.com>
2017-04-18 14:40:37 +0300

    Creating multiple chairs.

--- src/app/Components/class-room/class-room.component.ts
+++ src/app/Components/class-room/class-room.component.ts
@@ -4,6 +4,9 @@
 import {Component,OnInit} from "@angular/core";
 import {ActivatedRoute} from '@angular/router';
 
+
+import { ChairModel } from '../../models/chair.model';
+
 @Component({
   selector: 'class-room',
   templateUrl: './class-room.component.html',
@@ -12,12 +15,37 @@ import {ActivatedRoute} from '@angular/router';
 
 export class ClassRoomComponent implements OnInit{
   private chairsNumber: number;
-  constructor(private Route: ActivatedRoute){}
+  private chairs: Array<ChairModel>;
+
+  constructor(private Route: ActivatedRoute){
+    this.chairs = [];
+  }
 
   ngOnInit(){
+      this.retrieveParams();
+      this.createChairs();
+  }
+
+
+  private retrieveParams(){
     this.Route.params.subscribe((params) => {
-      this.chairsNumber = params['num'];
+      if(params['num'] > 20){
+        this.chairsNumber = 20;
+      }
+      else if(params['num'] < 5){
+        this.chairsNumber = 5;
+      }
+      else{
+        this.chairsNumber = params['num'];
+      }
     });
+  }
+
+  private createChairs(){
+    for(var i = 1; i <= this.chairsNumber; i++){
+      let chair = new ChairModel();
+      this.chairs.push(chair);
+    }
   }
 
 }
---
+++ src/app/models/chair.model.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by Lenovo on 18-Apr-17.
+ */
+
+export class ChairModel{
+  leftOffset: number;
+  topOffset: number;
+  studentName: string;
+}
